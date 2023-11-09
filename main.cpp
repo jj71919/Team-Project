f0e4fc5ed152fd104527683014256f6cb2cc7838
jj71919 <145038794+jj71919@users.noreply.github.com>
2023-11-09 11:41:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,35 @@
 #include "Player.h"
 using namespace std;
 
+bool operator < (const Player& lhs, const Player& rhs) {
+    return lhs.getCountry() < rhs.getCountry();
+}
+
+bool operator > (const Player& lhs, const Player& rhs) {
+    return lhs.getCountry() > rhs.getCountry();
+}
+
+
+template <typename TYPE>
+void bubbleSort(vector<TYPE> &z, int n){
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (z.at(j) > z.at(j+1)) {
+                swap(z.at(j), z.at(j+1));
+                swapped = true;
+            }
+        }
+ 
+        // If no two elements were swapped
+        // by inner loop, then break
+        if (swapped == false)
+            break;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     string input;
     string enteredFirstName;
@@ -17,19 +46,20 @@ int main(int argc, const char * argv[]) {
     //Create a vector that can hold player objects
     //Adds text from file into vector
     vector<Player> USA;
+    vector<Player> CZE;
     vector<Player> players;
     int rank, points;
     string lastName, firstName, country;
     while(!inFile.eof()){
         inFile >> rank >> lastName >> firstName >> country >> points;
         Player aPlayer(rank, lastName, firstName, country, points);
-        if(country == "USA"){
-            USA.push_back(aPlayer);
-        }
+        players.push_back(aPlayer);
+        
     }
+    int x = players.size();
+    bubbleSort(players,x);
     int i = 0;
-    for(i = 0; i < USA.size() ; i++){
-        USA.at(i).display();
+    for(i = 0; i < players.size() ; i++){
+        players.at(i).display();
     }
 }
-
