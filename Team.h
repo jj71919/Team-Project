1d4d9b332383accb65e7f676257adbeff322da49
jj71919 <145038794+jj71919@users.noreply.github.com>
2023-11-21 11:56:21 -0500

    Update Team.h

--- Team.h
+++ Team.h
@@ -18,7 +18,28 @@ public:
     string getCountry() const{
         return this->country;
     };
+    int getNumPlayers() const{
+        return numPlayers;
+    }
+    vector<Player> getTeamPlayer(){
+        return *teamPlayers;
+    }
+    void setPlayer(int index, vector<Player> vectPlayers){
+        teamPlayers->at(index).setPoints( vectPlayers.at(index).getPoints());
+    }
+    
+    
     //void addPointsToPlayer(string,string,int);
+    
+    Team(const Team& other) {
+            country = other.country;
+            numPlayers = other.numPlayers;
+            teamPlayers = new vector<Player>(*other.teamPlayers);
+        }
+    
+        ~Team() {
+            delete teamPlayers;
+        }
     
     
 private:
