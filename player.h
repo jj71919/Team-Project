b0335122ee124f73c9681f50dd6be56ba7cc7813
MattKeeler12 <145038844+MattKeeler12@users.noreply.github.com>
2023-11-21 12:08:00 -0500

    Update player.h

--- player.h
+++ player.h
@@ -1,56 +1,37 @@
-#ifndef Player_h
-#define Player_h
-#endif
+// Player.cpp
+// Matthew Keeler and Joe LaBarbera
+
+// This program creates a default constructor and the display sequence
+#include <stdio.h>
 #include <iostream>
 #include <string>
-#pragma once
-using namespace std;
+#include <vector>
+#include "Player.hpp"
 
-//Player.H
-class Player{
-    //public files that have default constructor
-  public:
-    Player();
-    Player(int, string, string, string, int );
-    string getLastName(){
-        return mlastName;
-    };
-    //Getters and Setters
-    string getfirstName() const{
-        return mfirstName;
-    }
+// Default that will put variables into the Player object
+Player::Player(int rank, string lastName, string firstName, string country, int points){
+    mrank = rank;
+    mlastName = lastName;
+    mfirstName = firstName;
+    mcountry = country;
+    mpoints = points;
+}
+Player::Player(){
+    
+}
+// Print method that will dispay the player object and corresponding variables
+void Player::display(){
+    cout << mfirstName << " " << mlastName << " " << mrank << " " << mcountry << " " << mpoints <<endl;
+}
+void Player::addPoints(int value){
+    mpoints += value;
+}
 
-    string getCountry() const{
-        return mcountry;
-    }
-    
-    int getPoints()const{
-        return mpoints;
-    }
-    
-    int getRank()const{
-        return mrank;
-    }
-    
-    Player operator+ (int);
-    
-    void display();
-    
-    void addPoints(int);
-
-    //void selectionSort(vector<Player> z);
-    
-  private:
-    //Private variables
-    string mlastName;
-    string mfirstName;
-    string mcountry;
-    int mrank;
-    int mpoints;
-};
-
-      
-
-      
-
+Player::Player(const Player &pPlayer){
+    this->mrank = pPlayer.mrank;
+    this->mfirstName = pPlayer.mfirstName;
+    this->mlastName = pPlayer.mlastName;
+    this->mcountry = pPlayer.mcountry;
+    this->mpoints = pPlayer.mpoints;
+}
 
